a97c4d68ab14bc1d6934101eb0cd3369ae43dfde
MartinG <m.gestefeld@jacobs-university.de>
2020-05-20 18:11:28 +0200

    now working correctly

--- agreement.py
+++ agreement.py
@@ -1,7 +1,13 @@
+def patternvector(H):
+    P=np.zeros(len(H))
+    for i,val in enumerate(H):
+        if val != 0:
+            P[i]=1
+    return P
+
 def Patternagreement(P):
     if max(P)>1:
         raise ValueError("Somethings wrong here")
-        
     K= len(P)
     TDU=0
     TU=0
@@ -13,19 +19,21 @@ def Patternagreement(P):
                 if P[i] == 1 and P[j] == 1 and P[m] == 0:
                     TU += 1   # 110 pattern, unimodal (TU)
                 if P[i] == 0 and P[j] == 1 and P[m] == 1:
-                    TU += 1   # 011 pattern, unimodal (TU)
-                    # all other patterns are not counted
-    if TU == 0 and TDU == 0:
-        return 1
+                    TU += 1   # 011 pattern, unimodal (TU)      
+    if TU == 0 and TDU == 0: #U=1
+        S = sum(P)
+        A= 1-(S-1)/(K-1) 
+    elif ((K-2)*(TU+TDU))==0:
+        A = 0
     else:
-        U = ((K-2)*TU-(K-1)*TDU)/((K-2)*(TU+TDU)) # normal case: U as in equation (2) on p.332
+        U = ((K-2)*TU-(K-1)*TDU)/((K-2)*(TU+TDU)) # normal case: U as in equation (2) on p.332      	
         S = sum(P)                        # number of non-empty
         A = U*(1-(S-1)/(K-1))             # calculating agreement A
-        if np.isnan(A):
+        if K==1:
             A = 0            # lack of agreement, defined as 0
         if sum(P) == 1:
             A = 1          # only one value, defined as 1
-        return A        
+    return A        
 
 
 def agreement(V):
@@ -35,19 +43,20 @@ def agreement(V):
         raise ValueError( "error")
     else:
         AA=0
-        A=[]
+        #A=[]
         k=len(V)
         N=sum(V)
         R=V
-        for i in V:
+        for i in range(k):
             P=patternvector(R)
             if max(P)==0:
                 break
             A=Patternagreement(P)
+            #print(R)
             m = np.ma.masked_equal(R, 0.0, copy=False).min()
             L=P*m
             w=sum(L)/N
-            AA=AA+w*A
-            R=R-L
+            AA+= w*A
+            R= R-L
     return AA
             
